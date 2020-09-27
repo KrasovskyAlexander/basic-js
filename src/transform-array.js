0f2322370621e0d8d0bafa13855e7440d893e0d4
Alexander Krasovsky <casha_krasovski@mail.ru>
2020-09-28 00:12:04 +0300

    feat: complete transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,21 +4,22 @@ module.exports = function transform(arr) {
   if (Array.isArray(arr)) {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == '--discard-prev' && result.length !== 0 && arr[i - 2] !== '--discard-next') {
+        if (arr[i] == '--discard-prev') {
+          if(result.length === 0 || arr[i-2]==='--discard-next') continue;
           result.pop();
-        }else  if (arr[i] == '--double-prev' && i !== 0 && arr[i - 2] !== '--discard-next') {
+        }else  if (arr[i] == '--double-prev') {
+          if(result.length === 0 || arr[i-2]==='--discard-next') continue;
           result.push(arr[i - 1]);
-        }else  if (arr[i] == '--double-next' && i < arr.length - 1){
+        }else  if (arr[i] == '--double-next'){
+          if( i === arr.length - 1) continue;
            result.push(arr[i + 1]);
         }else if(arr[i] == '--discard-next') {
           i++;
-        }
-        else{
+        }else{
           result.push(arr[i]);
         }
           
       };
-    
     return result;
   } else {
     throw new Error();
