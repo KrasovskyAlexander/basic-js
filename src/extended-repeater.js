7c0cf626ccb7d67aff4d00b09dddc4706c231f1e
Alexander Krasovsky <casha_krasovski@mail.ru>
2020-09-29 22:06:40 +0300

    feat: complete repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  return str+((options.addition+options.additionSeparator).repeat(options.additionRepeatTimes)).repeat(options.repeatTimes);
+module.exports = function repeater(str, {repeatTimes = 1,separator = '+',addition, additionRepeatTimes,additionSeparator =''}) {
+  str = ''+str;
+  if(addition === undefined){
+    addition='';
+  }
+  addition= ''+addition;
+  if(!additionRepeatTimes){
+    additionSeparator ='';
+    additionRepeatTimes =1;
+  }
+  let additions = (addition+additionSeparator).repeat(additionRepeatTimes);
+  additions = additions.slice(0,additions.length-additionSeparator.length);
+  let newStr = (str+additions)+separator;
+  newStr = newStr.repeat(repeatTimes);
+  return newStr.slice(0,newStr.length-separator.length);
 };
+
   
